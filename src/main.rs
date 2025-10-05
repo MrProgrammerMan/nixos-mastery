f142b6f4f7949f38d7a54c478455870527e0b9a6
MrProgrammerMan <jonas.baugerud@gmail.com>
2025-10-05 17:43:08 +0200

    feat(axum): set up basic axum web server listening at port 80

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "This response was sent from Axum!" }));
+    let listener =  tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
