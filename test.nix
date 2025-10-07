d51978d0925160b340bb0ffa770329f6f126a641
MrProgrammerMan <jonas.baugerud@gmail.com>
2025-10-07 20:50:26 +0200

    feat(nix): test.nix

---
+++ test.nix
@@ -0,0 +1,35 @@
+{ self, pkgs }:
+pkgs.nixosTest {
+    name = "Nixos mastery service test";
+
+    nodes = {
+        server = { config, ... }: {
+            imports = [ self.nixosModules.nixos-mastery ];
+
+            services.nixos-mastery = {
+                enable = true;
+                port = 3000;
+            };
+
+            networking.firewall.allowedTCPPorts = [
+                config.services.nixos-mastery.port
+            ];
+        };
+
+        client = {...}: {};
+    };
+
+    globalTimeout = 20;
+
+    testScript = { nodes, ... }: ''
+        PORT = ${builtins.toString nodes.server.services.nixos-mastery.port}
+        
+        start_all()
+
+        server.wait_for_unit("nixos-mastery.service")
+        server.wait_for_open_port(${builtins.toString nodes.server.services.nixos-mastery.port})
+        client.wait_for_unit("network-online.target")
+
+        output = client.succeed(f"curl -s http://server:{PORT}/")
+    '';
+}
